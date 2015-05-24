461a73de56e965239c74f2e834486f3f4614fc0e
Tomas Björnfot <tomas.bjornfoth@gmail.com>
2015-05-24 18:27:50 +0200

    Update Radio.py

--- Radio.py
+++ Radio.py
@@ -1,46 +1,48 @@
 import os
 import time
 class Radio:
-        def __init__(self):
-                self.stations = []
-                self.station = ''
+    def __init__(self):
+        self.stations = [] # all station URL's in the category
+        self.station = '' # the URL of the current station.
+        self.stationNumber = 1 # current station number (stations[n-1])
+        self.stationNames = []  # all station names in the category
+        self.stationCategory = [] # the current category
+        self.numberOfStations = 0 # the number of stations in the category
+    def startStation(self,station):
+        print "start station",station
+        self.setStation(station)
+        self.add()
+        self.play()
+    def stopStation(self):
+        print "stops station"
+        self.stop()
+        self.clear()
+    def nextStation(self):
+        self.stopStation()
+        self.stationNumber = self.stationNumber + 1;
+        if self.stationNumber > self.numberOfStations:
                 self.stationNumber = 1
-                self.numberOfStations = 0
-        def startStation(self,station):
-                print "start station",station
-                self.setStation(station)
-                self.add()
-                self.play()
-        def stopStation(self):
-                print "stops station"
-                self.stop()
-                self.clear()
-        def nextStation(self):
-                self.stopStation()
-                self.stationNumber = self.stationNumber + 1;
-                if self.stationNumber > self.numberOfStations:
-                        self.stationNumber = 1
-                self.startStation(self.stationNumber)
-        def readStations(self):
-                with open('stations','r') as f:
-                        for line in f:
-                                self.stations.append(line.rstrip())
-                                print "added",line.rstrip(),"to stations"
-                self.numberOfStations = len(self.stations)
-                print "number of stations",self.numberOfStations
-        def setStation(self,stationNumber):
-                self.station = self.stations[stationNumber-1]
-        def add(self):
-                os.system('mpc add '+self.station)
-        def play(self):
-                os.system('mpc play')
-        def stop(self):
-                os.system('mpc stop')
-        def clear(self):
-                os.system('mpc clear')
-        def setVolume(self,vol):
-                os.system('mpc volume '+str(vol))
-                print "volume set to",vol
-
-
-
+        self.startStation(self.stationNumber)
+    def readStations(self):
+        with open('stations','r') as f:
+                for line in f:
+                        line = line.rstrip()
+                        lineVector = line.split(',')
+                        self.stations.append(lineVector[0])
+                        self.stationNames.append(lineVector[1])
+                        self.stationCategory.append(lineVector[2])
+        self.numberOfStations = len(self.stations)
+        print "number of stations",self.numberOfStations
+    def setStation(self,stationNumber):
+        self.station = self.stations[stationNumber-1]
+    def add(self):
+        os.system('mpc add '+self.station)
+    def play(self):
+        os.system('mpc play')
+    def stop(self):
+        os.system('mpc stop')
+    def clear(self):
+        os.system('mpc clear')
+    def setVolume(self,vol):
+        os.system('mpc volume '+str(vol))
+        print "volume set to",vol
